76d32a8ccbb960e56fce7582ec85dcc84be5be4a
flynn940 <85081735+flynn940@users.noreply.github.com>
2021-05-31 09:36:34 +1200

    C:\xampp\htdocs\L2 web design
    learning html

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head> 
+	<meta charset "utf-8"> 
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>my new website</title>
+</head>
+
+<body>
+
+	<div id="wrapper">
+		<header>
+				example
+		</header>
+		
+		<nav>
+			<ul>
+				<li> <a href="index.html"> Home </a> </li>
+				<li>
+					<a> About
+						<ul class="hidden">
+							<li>What we do</li>
+							<li>About us</li>
+						</ul>
+					</a>
+				</li> 
+				<li><a href="contact.html"> contact </a> </li>"
+			</ul>
+			
+		</nav>
+		
+		<div id="content"></div>
+
+		<section class="flynn">
+			<h1>example</h1>
+			hhh
+		</section>
+		<footer>
+			footar
+		</footer>
+	</div>
+    <?php include"top.php"; ?>
+</body>
+
+
+<html>
+
+
+
